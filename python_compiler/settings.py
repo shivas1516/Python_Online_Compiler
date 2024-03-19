84704481b54a89273791d774fc9545d03d5cc36e
SIVASUBRAMANIAM L <141582345+shivas1516@users.noreply.github.com>
2024-03-19 22:20:27 +0000

    vercel config

--- python_compiler/settings.py
+++ python_compiler/settings.py
@@ -76,12 +76,13 @@ WSGI_APPLICATION = 'python_compiler.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 
 
 # Password validation
